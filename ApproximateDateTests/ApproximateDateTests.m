a91b86f95a31e2fc555be862a90ff82271c508f7
Andrew Velis <andrew.velis@gmail.com>
2013-07-30 00:22:41 -0700

    Fix to ApproximateDateTests target
    Previously it was in a state that was not testable. Currently the test
    spec is not visible. This will be fixed soon.

---
+++ ApproximateDateTests/ApproximateDateTests.m
@@ -0,0 +1,36 @@
+//
+//  ApproximateDateTests.m
+//  ApproximateDateTests
+//
+//  Created by Andrew Velis on 7/30/13.
+//  Copyright (c) 2013 Andrew Velis. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ApproximateDateTests : SenTestCase
+
+@end
+
+@implementation ApproximateDateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
