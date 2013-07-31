4d52c1ec95f730c53e4dc082b33caf82d649f7a0
Andrew Velis <andrew.velis@gmail.com>
2013-07-30 23:30:33 -0700

    Removal of extraneous header comment

--- ApproximateDate/NSDate+Approximate.h
+++ ApproximateDate/NSDate+Approximate.h
@@ -1,9 +1,6 @@
 //
 // Created by Andrew Velis on 7/30/13.
 // Copyright (c) 2013 Andrew Velis. All rights reserved.
-//
-// To change the template use AppCode | Preferences | File Templates.
-//
 
 
 #import <Foundation/Foundation.h>
